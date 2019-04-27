37619660ec57df7b4726c87e4ffc166109f607e5
Aadhar gaur (Windows 10) <aadhar41@gmail.com>
2019-04-27 17:09:53 +0530

    Laravel collective form open tag changed

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1>Create Todo  </h1>
-    {{ Form::open(['url' => 'foo/bar']) }}
-	//
+    {{ Form::open(['url' => 'foo/bar', 'method' => 'put']) }}
+
     {{ Form::close() }}
 @endsection
