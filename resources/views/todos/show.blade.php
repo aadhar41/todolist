1a29d4f01695ce84293c29629531a31d776c168c
Aadhar gaur (Windows 10) <aadhar41@gmail.com>
2019-04-28 10:30:22 +0530

    019 Delete Todos

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -5,10 +5,17 @@
   <a class="btn btn-primary" href="/">Go Back</a>
   <br><br>
   <div class="shadow-sm p-4 mb-4 bg-white ">
-  <h2><a href="/todo/{{$todo->id}}">{{$todo->text}}</a></h2>
-  <div class="badge badge-pill badge-danger">{{$todo->due}}</div>
-  <hr>
-  <p>{{$todo->body}}</p>
-  <a href="/todo/{{$todo->id}}/edit" class="btn btn-outline-primary">Edit</a>
+    <h2><a href="/todo/{{$todo->id}}">{{$todo->text}}</a></h2>
+      <div class="badge badge-pill badge-danger">{{$todo->due}}</div>
+      <hr>
+      <p>{{$todo->body}}</p>
+      <a href="/todo/{{$todo->id}}/edit" class="btn btn-outline-primary">Edit</a>
+      <br><br>
+      {{ Form::open(['action' => ['TodosController@destroy', $todo->id], 'method' => 'POST', 'class' => 'pull-right']) }}
+        
+        {{ Form::hidden('_method', 'DELETE') }}
+        {{ Form::bsSubmit('Delete',['class'=>'btn btn-outline-danger']) }}
+
+      {{ Form::close() }}
   </div>
 @endsection
